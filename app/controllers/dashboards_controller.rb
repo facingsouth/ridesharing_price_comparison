6795e41168f4dc7dc5f2cd7769d8ad96f36db2b9
facingsouth <xin.d.liu@gmail.com>
2015-08-15 08:38:27 -0700

    pt error msg

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,8 +6,9 @@ class DashboardsController < ApplicationController
   def create
     @origin = params[:origin]
     @destination = params[:destination]
-    uber = Uber.new
-    data = uber.uber_type_price_distance(@origin, @destination)
+    # uber = Uber.new
+    # data = uber.uber_type_price_distance(@origin, @destination)
+  data = []
     public_transit = PublicTransit.new(@origin, @destination, "transit")
     data << public_transit.data_parser
     driving = PublicTransit.new(@origin, @destination, "driving")
