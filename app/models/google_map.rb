b0594adbb7565c3547efdd0bdce693cf61356e63
alokp <alokpra@gmail.com>
2015-08-15 10:38:01 -0700

    map working

--- app/models/google_map.rb
+++ app/models/google_map.rb
@@ -11,18 +11,8 @@ class GoogleMap
     @destination = CGI::escape(Geocoder.address(destination))
   end
 
-  def geo_address(start_address)
-    @start_city = Geocoder.search(@origin).first
-    @start_state = Geocoder.search(start_address).first
-  end
-
-  def address_to_coordinates(user_address)
-    Geocoder.coordinates(user_address)
-  end
-
   def build_url
-    url_string = "https://www.google.com/maps/embed/v1/directions?origin=#{@origin}&destination=#{@destination}&key=AIzaSyBJHptJsjF1gs_K-Y595e9JmUx24VMQtgo"
-    url_string
+    "https://www.google.com/maps/embed/v1/directions?origin=#{@origin}&destination=#{@destination}&key=AIzaSyBJHptJsjF1gs_K-Y595e9JmUx24VMQtgo"
   end
 
 end
